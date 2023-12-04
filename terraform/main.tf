9e22abec6140ce485b0a1123d12cb7d6959bf957
Himanshu Sagar <hsagar60@gmail.com>
2023-12-04 19:31:55 +0530

    modification in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,7 +92,7 @@ resource "aws_security_group" "allow_web" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["10.0.0.1/32"]
     # ipv6_cidr_blocks = ["::/0"]
   }
 
